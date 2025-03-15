c727cd3d7259eaffa85a3843d98466e5f9953b47
bronsonh <bronson.hill@student.unimelb.edu.au>
2025-03-15 13:18:21 +1100

    Refactor logging in run.py to use os.write for status and error messages

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import streamlit as st
+import os
 from Home import main
 
 if __name__ == "__main__":
     # This script is a simple wrapper that calls the main function from Home.py
     # It's provided for convenience when running the app using `python run.py`
     try:
-        st.write("running main")
+        os.write("running main")
         main()
 
     except Exception as e:
-        st.write("log: " + e)
+        os.write("log: " + e)
