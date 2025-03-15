6c227d9c4e7b4560bd337e9d6eb49201e69b0756
bronsonh <bronson.hill@student.unimelb.edu.au>
2025-03-15 13:17:15 +1100

    Add status message to indicate when the main function is running in run.py

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     # This script is a simple wrapper that calls the main function from Home.py
     # It's provided for convenience when running the app using `python run.py`
     try:
+        st.write("running main")
         main()
 
     except Exception as e:
