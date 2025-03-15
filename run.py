d0852c2d5f5467b272fc3f94fb663d9066cb58a2
bronsonh <bronson.hill@student.unimelb.edu.au>
2025-03-15 13:25:51 +1100

    Replace os.write with print statements for better logging in run.py

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import os
+import sys
 from Home import main
 
 if __name__ == "__main__":
     # This script is a simple wrapper that calls the main function from Home.py
     # It's provided for convenience when running the app using `python run.py`
     try:
-        os.write("running main")
+        print("running main")
+        sys.stdout.flush()
         main()
 
     except Exception as e:
-        os.write("log: " + e)
+        print(e)
+        sys.stdout.flush()
